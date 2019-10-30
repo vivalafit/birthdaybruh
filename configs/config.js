bfb6af9e05e87900b29ecf5ba2dc32c28c5cb8a2
MAXXIMUS\Max <navrotskymax@gmail.com>
2019-10-30 09:25:15 +0200

    bot logic test

---
+++ configs/config.js
@@ -0,0 +1,7 @@
+const config = {
+    keys : {
+        TELEGRAM_BOT_API_KEY : process.env.TELEGRAM_BOT_API_KEY || 'bruh, no key'
+    }
+};
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const app = express();
+const port = 3000;
+const bot = require('./core/controllers/bot')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
